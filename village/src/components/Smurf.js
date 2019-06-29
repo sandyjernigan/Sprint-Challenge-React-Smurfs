52161268f5a5ab10431a2e5fb7738e7afd86813a
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-29 13:57:14 -0400

    Starting Stage 2

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -4,7 +4,7 @@ const Smurf = props => {
   return (
     <div className="Smurf">
       <h3>{props.name}</h3>
-      <p>ID: {props.id} -- <strong>{props.height} tall</strong> - 
+      <p><strong>{props.height} tall</strong> - 
       <span> {props.age} smurf years old</span></p>
     </div>
   );
