d8b63974e7d5b3b27da4e7c37df25152c8cdc397
sandyjernigan <sandy.jernigan@gmail.com>
2019-07-01 02:22:53 -0400

    changed font size on footer

--- village/src/components/Footer.js
+++ village/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function () {
   return (
     <footer>
-      Images thanks to:  <a href="https://www.vecteezy.com">Vector Design by www.Vecteezy.com</a> 
+      <p>Images thanks to:  <a href="https://www.vecteezy.com">Vector Design by www.Vecteezy.com</a></p>
     </footer>
   )
 }
